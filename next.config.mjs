e3a7bd34ada8c2219f84f62231c77a696d7a7764
hichemboutaba1 <hichemboutaba1@gmail.com>
2026-04-05 20:55:10 +0200

    Delete next.config.mjs

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    serverComponentsExternalPackages: ["pdf-parse", "exceljs", "pptxgenjs", "docx", "@mistralai/mistralai"],
-  },
-};
-
-export default nextConfig;
